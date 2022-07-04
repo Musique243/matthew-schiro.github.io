5cb8eca8a40fd24a2751f07419e60a717549426d
Matthew S <matthewjschiro@gmail.com>
2022-07-04 16:58:10 +0000

    recursion, dup

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -337,6 +337,14 @@ var flatten = function(arrays) {
 // 30. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {'p':1, 'o':2, 't':2, 'a':1}
 var letterTally = function(str, obj) {
+  if(str.length === 0) {
+    return obj;
+  }else if(obj[str.charAt(0)]) {
+    obj[str.charAt(0)]++;
+  }else{
+    obj[str.charAt(0)] = 1;
+  }
+  return letterTally(str.slice(1), obj);
 };
 
 // 31. Eliminate consecutive duplicates in a list.  If the list contains repeated
@@ -344,7 +352,18 @@ var letterTally = function(str, obj) {
 // elements should not be changed.
 // Example: compress([1, 2, 2, 3, 4, 4, 5, 5, 5]) // [1, 2, 3, 4, 5]
 // Example: compress([1, 2, 2, 3, 4, 4, 2, 5, 5, 5, 4, 4]) // [1, 2, 3, 4, 2, 5, 4]
-var compress = function(list) {
+var compress = function(list, result = []) {
+  if(list.length === 0) {
+    return result;
+  }
+  if(list.length === 1) {
+    result.push(list[0]);
+    return result;
+  }
+  if(list[0] !== list[1]) {
+    result.push(list[0]);
+  }
+  return compress(list.slice(1), result);
 };
 
 // 32. Augment every element in a list with a new value where each element is an array
