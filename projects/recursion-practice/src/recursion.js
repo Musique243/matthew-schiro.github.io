9ed060cc6260378b646e89b1cb8bc1511249965f
Matthew S <matthewjschiro@gmail.com>
2022-07-05 20:59:07 +0000

    recursion, compare

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -174,8 +174,12 @@ var gcd = function(x, y) {
 var compareStr = function(str1, str2) {
   if(str1.length === 0 && str2.length === 0) {
     return true;
-  
   }
+  if(str1[0] === str2[0]) {
+    return compareStr(str1.slice(1), str2.slice(1));
+  }
+    return false;
+  
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
