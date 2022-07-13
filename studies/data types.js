3603f297ea3b31278a098aff2fabd99b295f1593
Matthew S <matthewjschiro@gmail.com>
2022-07-13 03:57:51 +0000

    studies, data

--- studies/data types.js
+++ studies/data types.js
@@ -11,6 +11,29 @@
       // 4. NaN: refers to not a number, when a value represented is not a number //
       // 5. Undefined: refers to not having a value or the value being undefined //
       // 6. Null: Represents the intentional absence of any value and is treated as falsy for boolean operations //
+*/
+
+let greeting = "Hello there";
+console.log(greeting); //prints Hello there
+
+let total = 40
+function add() {
+      total + 1;
+}
+console.log(add()); // prints 41
+
+10 === 10; // true
+var canVote = true;
+var isGreater = 5 > 10; // false
+
+let test;
+console.log(test); // undefined
+
+let x = -7;
+if(x > 0) {
+      console.log("valid number")
+}
+return null;
 
 // 2. Complex Data-types //
 // Data-types can hold a collection of data and more complex entities. These data-types are copied by reference, meaning when assigned they are passed by reference and not by value. Complex data-types can also be of any size. Objects, Arrays, and Functions are all complex data-types. //
@@ -23,6 +46,16 @@
       addNum(1, 2);
     console.log(addNum(1, 2)) Prints 3 to the console
 */
+
+let names = ['Frank', 'Jean', 'Kelly']
+console.log(names[1]); //prints Jean
+
+let object = {
+      name: "Ted",
+      age: 50,
+      email: "tedis50@aol.com"
+}
+console.log(object.name); // prints Ted
 
 // 3. Infinity and Negative Infinity //
 // Infinity is a numeric value that represents positive infinity. Any positive value multiplied by infinity will equal infinity. Any positive number divided by infinity will equal zero and will also hold a true value. Negative infinity represents the negative infinity value. Any positive value multiplied by negative infinity (including infinity) will equal negative infinity. Negative infinity divided by negative value (except negative infinity) will equal positive infinity. //
